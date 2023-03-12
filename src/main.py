a628cb2520950654bc9c61abc2f89aa7ef1b055b
Kynan Ware <47394200+BagToad@users.noreply.github.com>
2023-03-11 18:45:20 -0700

    potential fix for flip()

--- src/main.py
+++ src/main.py
@@ -161,9 +161,7 @@ async def randomize_all(ctx, n: int=2):
     await ctx.send(f"Okay, I'm doing it!\n{member_list}")
 
 @bot.command(name="flip", help="[flip]")
-async def flip(ctx, member: discord.Member):
-    if not member:
-        member = ctx.author
+async def flip(ctx, member: discord.Member=ctx.author):
     words = member.nick.split()
     reversed_words = words[::-1]
     r = await set_name(ctx.author, ' '.join(reversed_words))
